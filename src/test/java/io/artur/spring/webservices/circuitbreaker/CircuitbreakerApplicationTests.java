95a84f646e40fb1c140e5249639a90e449f487fa
Artur Przychodzen <artur2p@gmail.com>
2021-11-09 09:50:47 +0100

    init

---
+++ src/test/java/io/artur/spring/webservices/circuitbreaker/CircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.webservices.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
